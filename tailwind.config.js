6ad8e52f1ea746f67c0a9253531146293f53ec0a
thelastjeremiah <thelastgaviola@gmail.com>
2023-04-15 12:49:43 +0800

    home page finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ['./page/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'facebook': '#3B5998',
+        'gmail': '#D44638',
+      },
+      backgroundColor: {
+        'main-cover': '#03a9f426',
+        'main-cover-mobile':'#1516168a'
+        // #1516168a
+      },
+    },
   },
   plugins: [],
 }
